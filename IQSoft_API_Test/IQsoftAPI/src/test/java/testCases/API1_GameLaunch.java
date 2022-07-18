726d6fe4ec989caa2803bd1bae1477bb26752a6e
Gor Babloyan <g.babloyan@iqsoft.am>
2022-07-18 17:48:15 +0400

    request body changes

--- IQSoft_API_Test/IQsoftAPI/src/test/java/testCases/API1_GameLaunch.java
+++ IQSoft_API_Test/IQsoftAPI/src/test/java/testCases/API1_GameLaunch.java
@@ -1,30 +1,43 @@
 package testCases;
 
+import com.google.gson.Gson;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import io.qameta.allure.Description;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import testData.APISVariables_GameLunching_Common;
-import testData.APIVariables_GameLaunching;
+import testData.APISVariables_GameLunching_RequestBody;
+import testData.APIVariables_GameLaunching_Response;
 
 public class API1_GameLaunch extends BaseTest {
-    APIVariables_GameLaunching gameLaunching = new APIVariables_GameLaunching();
-    APISVariables_GameLunching_Common apisVariables_gameLunching_common = new APISVariables_GameLunching_Common();
+    APIVariables_GameLaunching_Response gameLaunching = new APIVariables_GameLaunching_Response();
+    APISVariables_GameLunching_RequestBody apisVariables_gameLunching_Request_body = new APISVariables_GameLunching_RequestBody();
     int partnerID = 51;
     @BeforeClass
     public void setup() throws UnirestException, JSONException {
-        Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest.post(apisVariables_gameLunching_common.urlClient51)
-                .header("Content-Type", "application/json")
-                .body("{\r\n  \"PartnerId\": 51,\r\n  \"LanguageId\": \"en\",\r\n  \"Token\": \"8e7e58ad-a241-4f82-918e-6be53a3e5481\",\r\n  \"IsForMobile\": false,\r\n  \"GameId\": 21001,\r\n      \"Domain\": \"betfaro.com\"\r\n    }\r\n\r\n")
-                .asString();
+        Gson gson = new Gson();
 
+        String url = "https://production.iqsoftllc.com/51/api/Integration/OpenGame";
+
+        apisVariables_gameLunching_Request_body.setPartnerId(51);
+        apisVariables_gameLunching_Request_body.setLanguageId("en");
+        apisVariables_gameLunching_Request_body.setToken("8e7e58ad-a241-4f82-918e-6be53a3e5481");
+        apisVariables_gameLunching_Request_body.setIsForMobile("false");
+        apisVariables_gameLunching_Request_body.setGameId(21001);
+        apisVariables_gameLunching_Request_body.setDomain("betfaro.com");
+        String requestBodyValue = gson.toJson(apisVariables_gameLunching_Request_body);
+
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response = Unirest.post(url)
+                .header("Content-Type", "application/json")
+                .body(requestBodyValue)
+                .asString();
+        logger.info("Game Launching API request was sent");
 
         // Response body
         JSONObject responseBody = new JSONObject(response.getBody());
@@ -39,54 +52,58 @@ public class API1_GameLaunch extends BaseTest {
 
         System.out.println();
 
-
-
-
-//        String total = body.getString("total");
-//        JSONArray data = body.getJSONArray("data");
-//        String firstOne = String.valueOf(data.get(0));
-//        JSONObject ArrayFirst = new JSONObject(firstOne);
-//        String id = ArrayFirst.getString("id");
-//        String email = ArrayFirst.getString("email");
-//        System.out.println("Total--->  " + total);
-//        System.out.println();
-//        System.out.println("ID--->  " + id);
-//        System.out.println();
-//        System.out.println("Email--->  " + email);
-
-        // Response headers
-//        JSONObject headers = new JSONObject(response.getHeaders());
-//        String date = headers.getString("date");
-//        System.out.println("Header date --->" + date);
-
     }
 
+
+
+
+
     @Test(priority = 1)
-    @Description("Verify game launching API_s Status cod equals 200")
+    @Description("Verify game launching API_s Status cod (need to be 200)")
     public void codTest() {
+
         Assert.assertEquals(gameLaunching.getCod(), 200);
+        logger.info("Game Launching API Response status was checked");
+        logger.info("---------------------------------------------------------------");
     }
 
     @Test(priority = 2)
-    @Description("Verify game launching API_s Response cod equals 0")
+    @Description("Verify game launching API_s Response cod  (need to be 0)")
     public void responseCodeTest() {
         Assert.assertEquals(gameLaunching.getResponseCode(), 0);
+        logger.info("Game Launching API Response Cod was checked");
+        logger.info("---------------------------------------------------------------");
     }
 
     @Test(priority = 3)
-    @Description("Verify game launching API_s Response Description is null")
+    @Description("Verify game launching API_s Response Description is (need to be null)")
     public void responseDescriptionTest() {
         Assert.assertEquals(gameLaunching.getDescription(), null);
+        logger.info("Game Launching API Response Error Description was checked");
+        logger.info("---------------------------------------------------------------");
     }
 
     @Test(priority = 4)
-    @Description("Verify game launching API_s Response Object not null")
+    @Description("Verify game launching API_s Response Object (need not be null)")
     public void responseObjectTest() {
-        System.out.println();
         Assert.assertNotEquals(gameLaunching.getResponseObject(), "null");
-        System.out.println();
+        logger.info("Game Launching API Response Object was checked");
+        logger.info("---------------------------------------------------------------");
     }
 
 
+
+
+
+
+
+
+
+
+
+    @AfterClass
+    public void tearDown(){
+
+    }
 
 }
--- IQSoft_API_Test/IQsoftAPI/src/test/java/testData/APISVariables_GameLunching_Common.java
+++
@@ -1,5 +0,0 @@
-package testData;
-
-public class APISVariables_GameLunching_Common {
-   public String urlClient51 = "https://production.iqsoftllc.com/51/api/Integration/OpenGame";
-}
